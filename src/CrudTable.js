cb7457c9a908f2013305c5e90bf62193b708db0c
riddhi <rchakraborty@entiovi.com>
2023-10-12 14:56:12 +0530

    added delete functionality

--- src/CrudTable.js
+++ src/CrudTable.js
@@ -1,18 +1,36 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
 import {
+  DataGrid,
   GridToolbarContainer,
   GridToolbarColumnsButton,
   GridToolbarFilterButton,
   GridToolbarExport,
   GridToolbarDensitySelector,
 } from '@mui/x-data-grid';
-import { Button,Typography, TextField } from '@mui/material';
+import { Button,Typography } from '@mui/material';
 import AddUser from './AddUser';
 
 export default function CrudTable() {
+  const [data,setData]= React.useState([
+    { id: 1, action:'', username: 'riddhi', role: 'React.js', fullname:'Riddhideb Chakraborty', email:'rchakraborty@entiovi.com', profileimage:'NA', createdat:'12/10/2023'},
+    { id: 2, action:'', username: 'admin1', role: 'Node.js', fullname:'admin1', email:'admin1@gmail.com', profileimage:'--', createdat:'NA'},
+    { id: 3, action:'', username: 'admin2', role: 'Frontend', fullname:'admin2', email:'admin2@gmail.com', profileimage:'--', createdat:'NA'},
+    { id: 4, action:'', username: 'admin3', role: 'Backend', fullname:'admin3', email:'admin3@gmail.com', profileimage:'--', createdat:'NA'},
+    { id: 5, action:'', username: 'admin4', role: 'UI/UX', fullname:'admin4', email:'admin4@gmail.com', profileimage:'--', createdat:'NA'},
+  ]);
+
+  const handleDelete = (id) => {
+    const updatedData = data.filter((item) => item.id !== id);
+    setData(updatedData);
+  };
+  
 const columns = [
-  { field: 'action', headerName: 'Action', width: 250 },
+  { field: 'action', headerName: 'Action', width: 250, renderCell:(params)=>(
+    <div style={{display:"flex",gap:"10px"}}>
+    <Button variant='contained'>Edit</Button>
+    <Button variant='contained' color='error' onClick={() => handleDelete(params.row.id)}>Delete</Button>
+    </div>
+  ) },
   { field: 'username', headerName: 'Username', width: 250 },
   { field: 'role', headerName: 'Role', width: 250 },
   
@@ -28,22 +46,18 @@ const columns = [
   { field: 'createdat', headerName: 'Created At', width: 250 },
 ];
 
-const rows = [
-  { id: 1, action:'', username: 'riddhi', role: 'React.js', fullname:'Riddhideb Chakraborty', email:'rchakraborty@entiovi.com', profileimage:'NA', createdat:'12/10/2023'},
-  { id: 2, action:'', username: 'admin1', role: 'Node.js', fullname:'admin1', email:'admin1@gmail.com', profileimage:'NA', createdat:'NA'},
-  { id: 3, action:'', username: 'admin2', role: 'Frontend', fullname:'admin2', email:'admin2@gmail.com', profileimage:'NA', createdat:'NA'},
-  { id: 4, action:'', username: 'admin3', role: 'Backend', fullname:'admin3', email:'admin3@gmail.com', profileimage:'NA', createdat:'NA'},
-  { id: 5, action:'', username: 'admin4', role: 'UI/UX', fullname:'admin4', email:'admin4@gmail.com', profileimage:'NA', createdat:'NA'},
-];
 
 const CustomToolbar=()=> {
   return (
-    <GridToolbarContainer>
-      <GridToolbarColumnsButton />
-      <GridToolbarFilterButton />
-      <GridToolbarDensitySelector />
-      <GridToolbarExport />
+    <>
+    <GridToolbarContainer style={{backgroundColor:"black"}}>
+      <GridToolbarColumnsButton style={{color:"white"}}/>
+      <GridToolbarFilterButton style={{color:"white"}}/>
+      <GridToolbarDensitySelector style={{color:"white"}}/>
+      <GridToolbarExport style={{color:"white"}}/>
     </GridToolbarContainer>
+    
+    </>
   );
 }
 
@@ -58,9 +72,9 @@ const CustomToolbar=()=> {
       </div>
      
       
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: '100vh', width: '100vw' }}>
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
         slots={{
           toolbar: CustomToolbar,
@@ -70,7 +84,8 @@ const CustomToolbar=()=> {
             paginationModel: { page: 0, pageSize: 5 },
           },
         }}
-        pageSizeOptions={[5, 10]}/>
+        pageSizeOptions={[5, 10]}>
+        </DataGrid>
     </div>
     </>
   );
